5688011088d1c7190c414d1bad29fc38cad08cd8
panwarvipul00 <panwarvipul1234@gmail.com>
2025-05-06 14:00:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,11 @@
 function handleSubmit(event) {
       event.preventDefault(); // Prevent form submission
 
-      // Get values from form
       const firstName = document.querySelector('input[name="First Name"]').value;
       const lastName = document.querySelector('input[name="Last Name"]').value;
       const phoneNumber = document.querySelector('input[name="Phone Number"]').value;
       const emailID = document.querySelector('input[name="Email ID"]').value;
 
-      // Create alert message
       const message = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailID}`;
       alert(message);
+    }
